dcae66eedad8380e59124ec729d24cea849a6851
anmol saxena <anmolsaxena30191@gmail.com>
2023-03-30 02:15:52 +0530

    initial

---
+++ public/script/birdPage.js
@@ -0,0 +1,25 @@
+
+function fun2(){
+    document.getElementById('box').style.color = "black";
+    document.getElementById('box').style.backgroundColor = "white";
+}
+
+function fun1(){
+    let x = document.querySelector('#box');
+    let ans = x.value.toLowerCase();
+    console.log('afd');
+
+    if(ans === 'raftar' || ans === 'raftaar'){
+        x.style.backgroundColor = 'green';
+        x.style.color = 'white';
+
+        document.querySelector('#box').disabled = true;
+        document.querySelector('#butn').disabled = true;
+
+        setTimeout(()=>{window.location.href = "http://localhost:4000/forthPage";},5000);
+    }
+    else{
+        x.style.backgroundColor = 'red';
+        x.style.color = 'white';
+    }
+}
